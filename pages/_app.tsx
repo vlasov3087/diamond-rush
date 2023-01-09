4ad59e537cc7143a85218db254e42e3282d01948
Vlasov Andrey <andreivlasov11@gmail.com>
2023-01-09 04:31:49 +0300

    loader fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEventListener } from "../hooks/useEventListener";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const loader = document.getElementById("globalLoader");
-      if (loader) loader.style.display = "none";
-    }
-  }, []);
+  const handleLoaded = (): void => {
+    const loader = document.getElementById("globalLoader");
+    if (loader) loader.style.display = "none";
+  };
+  useEventListener(undefined, "scroll", handleLoaded);
+
   return <Component {...pageProps} />;
 }
