db1e1fd3efe7cf4d39355616931464b335e7ef91
Vlasov Andrey <andreivlasov11@gmail.com>
2023-01-14 00:02:21 +0300

    source map

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const shouldAnalyzeBundles = process.env.ANALYZE === 'true';
 /** @type {import('next').NextConfig} */
 let nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  }
+  productionBrowserSourceMaps: true,
 }
 if (shouldAnalyzeBundles) {
   const withNextBundleAnalyzer =
